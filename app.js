0a4c419dfb6274ee616373635a1749fad8d65328
Kenny Burgher <kenny.burgher@gmail.com>
2023-04-11 17:48:28 -0700

    changed div to img; refactoring

--- app.js
+++ app.js
@@ -2,33 +2,38 @@
 
 console.log("Let's get this party started!");
 
-// When the user submits the form, use axios to make a request to GIPHY for information based on that term.
-$("#searchForm").on("submit", getGiphyResponse);
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const BASE_API_URL = "http://api.giphy.com/v1";
+
+
+// When the user submits the form
+// use axios to make a request to GIPHY for information based on that term.
+$("#searchForm").on("submit", handleSubmit);
 $("#removeButton").on("click", removeGifs);
 
-async function getGiphyResponse(evt) {
+async function handleSubmit(evt) {
   evt.preventDefault();
 
-  const q = $("#searchInput").val();
-  const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+  const query = $("#searchInput").val();
+  const response = await getResponse(query);
 
-  const response = await axios.get(" http://api.giphy.com/v1/gifs/search", {
-    params: { q, api_key },
+  showGif(response.data, query);
+}
+
+async function getResponse(q) {
+  const response = await axios.get(BASE_API_URL + "/gifs/search", {
+    params: { q, api_key: API_KEY },
   });
-
-  // After receiving the response, console.log the response data to make sure you’re getting back what you expect.
-  console.log("response.data=", response.data);
-  showGif(response.data, q);
+  return response;
 }
 
 //grab a GIF from the response data and append the GIF to the page
-function showGif(response, q) {
-  const gifIndex = $(`.${q}`).length;
-  const gifUrl = response.data[gifIndex].url;
-  const $gifDiv = $().add(`<div class=${q}></div>`)
-    .css("background-image", `url(${gifUrl})`);
-  console.log($gifDiv.get())
-  $(".canvas").append($gifDiv);
+function showGif(response, query) {
+  // NOTE: Can use a Set of indices as alternative to prevent duplicate images
+  const gifIndex = $(`.${query}`).length;
+  const gifUrl = response.data[gifIndex].images.original.url;
+
+  $(".canvas").append(`<img class=${query} src=${gifUrl}/>`);
 }
 
 // Add a button next to the form which,
@@ -36,4 +41,3 @@ function showGif(response, q) {
 function removeGifs() {
   $(".canvas").empty();
 }
-
