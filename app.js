0c8fdbd2b579fda988d6c2ab94c839cd0bfb984e
Kenny Burgher <kenny.burgher@gmail.com>
2023-04-11 17:09:58 -0700

    ready for code review; CORS error

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log("Let's get this party started!");
 
 // When the user submits the form, use axios to make a request to GIPHY for information based on that term.
 $("#searchForm").on("submit", getGiphyResponse);
+$("#removeButton").on("click", removeGifs);
 
 async function getGiphyResponse(evt) {
   evt.preventDefault();
@@ -17,14 +18,22 @@ async function getGiphyResponse(evt) {
 
   // After receiving the response, console.log the response data to make sure you’re getting back what you expect.
   console.log("response.data=", response.data);
-
-  showGif(response.data);
+  showGif(response.data, q);
 }
 
 //grab a GIF from the response data and append the GIF to the page
-
-function showGif(responseData, q) {
-  for (let object of responseData) {
-    let gifUrl = object.url;
-  }
+function showGif(response, q) {
+  const gifIndex = $(`.${q}`).length;
+  const gifUrl = response.data[gifIndex].url;
+  const $gifDiv = $().add(`<div class=${q}></div>`)
+    .css("background-image", `url(${gifUrl})`);
+  console.log($gifDiv.get())
+  $(".canvas").append($gifDiv);
 }
+
+// Add a button next to the form which,
+// when clicked, will remove all GIFs from the page.
+function removeGifs() {
+  $(".canvas").empty();
+}
+
